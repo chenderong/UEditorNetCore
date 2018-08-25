040c5b98611577462c521541418f5441d8043797
钟召威 <zhongzhaowei@outlook.com>
2018-08-25 19:30:39 +0800

    UEditorNetCore

---
+++ UEditorNetCore/UEditorService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UEditorNetCore.Handlers;
+
+namespace UEditorNetCore
+{
+    public class UEditorService
+    {
+        private UEditorActionCollection actionList;
+
+        public UEditorService(IHostingEnvironment env, UEditorActionCollection actions)
+        {
+            Config.WebRootPath = env.WebRootPath;
+            actionList = actions;
+        }
+
+        public void DoAction(HttpContext context)
+        {
+            var action = context.Request.Query["action"];
+            if (actionList.ContainsKey(action))
+                actionList[action].Invoke(context);
+            else
+                new NotSupportedHandler(context).Process();
+        }
+    }
+}
---
+++ UEditorNetCoreExample/Controllers/UEditorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UEditorNetCore;
+
+namespace UEditorNetCoreExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UEditorController : ControllerBase
+    {
+        private UEditorService ue;
+        public UEditorController(UEditorService ue)
+        {
+            this.ue = ue;
+        }
+
+        public void Do()
+        {
+            ue.DoAction(HttpContext);
+        }
+    }
+}
